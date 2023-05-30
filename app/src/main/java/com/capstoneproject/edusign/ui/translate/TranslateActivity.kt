5e7a0129937484064f28642b9d86730be217aa13
rifky-22 <rifky.misbachudin22@gmail.com>
2023-05-30 14:24:20 +0700

    .

---
+++ app/src/main/java/com/capstoneproject/edusign/ui/translate/TranslateActivity.kt
@@ -0,0 +1,11 @@
+package com.capstoneproject.edusign.ui.translate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TranslateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_translate)
+    }
+}
